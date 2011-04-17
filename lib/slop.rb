e9e04e1595dda0e526b6841ad6c2c344dd2a6c35
Lee Jarvis <lee@jarvis.co>
2011-04-17 21:46:03 +0100

    bump version for bugfix
    this fix ensures tests ran using rubygems-test run green

--- lib/slop.rb
+++ lib/slop.rb
@@ -16,7 +16,7 @@ class Slop
   class InvalidOptionError < RuntimeError; end
 
   # @return [String] The current version string
-  VERSION = '1.5.0'
+  VERSION = '1.5.1'
 
   # Parses the items from a CLI format into a friendly object.
   #
