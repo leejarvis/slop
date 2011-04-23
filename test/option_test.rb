7457688eb1551135cbabf0313a14dad6a299d003
Lee Jarvis <lee@jarvis.co>
2011-04-23 23:00:35 +0100

    add test for abbreviated :sym

--- test/option_test.rb
+++ test/option_test.rb
@@ -63,6 +63,7 @@ class OptionTest < TestCase
   test 'casting' do
     assert_equal :foo, option_value(%w/--name foo/, :name, true, :as => Symbol)
     assert_equal :foo, option_value(%w/--name foo/, :name, true, :as => :symbol)
+    assert_equal :foo, option_value(%w/--name foo/, :name, true, :as => :sym)
     assert_equal 30, option_value(%w/--age 30/, :age, true, :as => Integer)
     assert_equal "1.0", option_value(%w/--id 1/, :id, true, :as => Float).to_s
   end
