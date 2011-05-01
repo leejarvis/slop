78a6272ff520622c19730a5c8494599bc0aa3064
Lee Jarvis <lee@jarvis.co>
2011-05-01 23:58:15 +0100

    bump bugfix version

--- lib/slop.rb
+++ lib/slop.rb
@@ -16,7 +16,7 @@ class Slop
   class InvalidOptionError < RuntimeError; end
 
   # @return [String] The current version string
-  VERSION = '1.5.3'
+  VERSION = '1.5.4'
 
   # Parses the items from a CLI format into a friendly object.
   #
