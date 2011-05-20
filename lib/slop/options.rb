f6bd8773099fc1f36e771ccf23b6b499e5c359b9
Lee Jarvis <lee@jarvis.co>
2011-05-20 19:41:43 +0100

    add some documentation to Slop::Options#[]

--- lib/slop/options.rb
+++ lib/slop/options.rb
@@ -13,7 +13,13 @@ class Slop
       end
     end
 
-    # Fetch an Option object
+    # Fetch an Option object. This method overrides Array#[] to provide
+    # a nicer interface for fetching options via their short or long flag.
+    # The reason we don't use a Hash here is because an option cannot be
+    # identified by a single label. Instead this method tests against
+    # a short flag first, followed by a long flag. When passing this
+    # method an Integer, it will work as an Array usually would, fetching
+    # the Slop::Option at this index.
     #
     # @param [Object] flag The short/long flag representing the option
     # @example
