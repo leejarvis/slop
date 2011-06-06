3433deed96139b4e7887ecc359319c8ed368f57f
Lee Jarvis <lee@jarvis.co>
2011-06-06 20:44:17 +0100

    enough of turn

--- test/helper.rb
+++ test/helper.rb
@@ -6,11 +6,6 @@ end
 require 'minitest/autorun'
 require 'stringio'
 
-begin
-  require 'turn'
-rescue LoadError
-end
-
 class TestCase < MiniTest::Unit::TestCase
   def self.test(name, &block)
     test_name = "test_#{name.gsub(/\s+/, '_')}".to_sym
