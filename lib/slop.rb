e77cd84e49aea7eb5383c1d59480c8aa39119f3b
Lee Jarvis <lee@jarvis.co>
2012-01-27 01:53:16 +0000

    bump version to 3.0.1

--- lib/slop.rb
+++ lib/slop.rb
@@ -4,7 +4,7 @@ require 'slop/commands'
 class Slop
   include Enumerable
 
-  VERSION = '3.0.0'
+  VERSION = '3.0.1'
 
   # The main Error class, all Exception classes inherit from this class.
   class Error < StandardError; end
