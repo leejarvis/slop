a1ee2866e2a350515992efd5a8da18109d5d9f3a
Lee Jarvis <ljjarvis@gmail.com>
2013-08-29 15:04:26 +0100

    Remove unnecessary extract methods

--- lib/slop/option_builder.rb
+++ lib/slop/option_builder.rb
@@ -5,8 +5,8 @@ class Slop
       def build(command, args, &block)
         short       = extract_flag(args, /\A\-?([a-z])(=)?\z/)
         long        = extract_flag(args, /\A\-?-?([a-zA-Z][a-zA-Z_-]+)(=)?\z/)
-        description = extract_description(args)
-        config      = extract_config(args)
+        description = args[0].respond_to?(:to_str) ? args.shift.to_str : nil
+        config      = args.shift || {}
 
         # default config options extracted from flags
         config[:argument] = @argument
@@ -22,16 +22,6 @@ class Slop
           @argument = $2 == '='
           $1
         end
-      end
-
-      def extract_description(args)
-        if args[0].respond_to?(:to_str)
-          args.shift.to_str
-        end
-      end
-
-      def extract_config(args)
-        args.shift || {}
       end
 
     end
