05cc99be8fcaec86a51c021f8b8245a549a50288
Lee Jarvis <ljjarvis@gmail.com>
2014-12-17 14:30:52 +0000

    Add NotImplementedError and docs

--- lib/slop/error.rb
+++ lib/slop/error.rb
@@ -1,7 +1,20 @@
 module Slop
+  # Base error class.
   class Error < StandardError
   end
 
-  class MissingArgument < Error; end
+  # Raised when calling `call` on Slop::Option (this
+  # method must be overriden in subclasses)
+  class NotImplementedError < Error
+  end
+
+  # Raised when an option that expects an argument is
+  # executed without one. Suppress with the `suppress_errors`
+  # config option.
+  class MissingArgument < Error
+  end
+
+  # Raised when an unknown option is parsed. Suppress
+  # with the `suppress_errors` config option.
   class UnknownOption   < Error; end
 end
