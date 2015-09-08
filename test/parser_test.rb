leejarvis/slop

commit ebe678b1639654604b3e54b08ca710c38682b83e

Use each_with_index for looping over arguments

lib/slop/parser.rb
@@ -62,8 +62,9 @@ module Slop
           # arguments (plus the arg if necessary)
           # delete argument first so that it doesn't mess up the index
           if opt.expects_argument?
-            index = arg_index(flag, arg)
-            arguments.delete_at(index) if !index.nil?
+            arguments.each_with_index do |argument, i|
+              arguments.delete_at(i + 1) if argument == flag
+            end
           end
           arguments.delete(flag)
         end
@@ -119,17 +120,6 @@ module Slop
 
     def matching_option(flag)
       options.find { |o| o.flags.include?(flag) }
-    end
-
-    # Returns the index of the argument corresponding to a flag.
-    def arg_index(flag, arg)
-      flag_index = arguments.index(flag)
-      return nil if flag_index.nil?
-
-      arg_index = arguments[flag_index..-1].index(arg)
-      return nil if arg_index.nil?
-
-      flag_index + arg_index
     end
   end
 end

commit 3df82ee30f7330d8137001c39737c5d7ebf6fcab

Fix bug regarding --foo=bar style options
This fixes the bug introduced in ebe678b1639654604b3e54b08ca710c38682b83e
where the argument after a flag is naively deleted. For example, with

    --foo=bar baz

, both `--foo=bar' and `baz' are deleted when only `--foo=bar' should be
deleted.

lib/slop/parser.rb
@@ -53,6 +53,7 @@ module Slop
         end
 
         # support `foo=bar`
+        orig_flag = flag.dup
         if flag.include?("=")
           flag, arg = flag.split("=")
         end
@@ -60,13 +61,15 @@ module Slop
         if opt = try_process(flag, arg)
           # since the option was parsed, we remove it from our
           # arguments (plus the arg if necessary)
-          # delete argument first so that it doesn't mess up the index
+          # delete argument first while we can find its index.
           if opt.expects_argument?
             arguments.each_with_index do |argument, i|
-              arguments.delete_at(i + 1) if argument == flag
+              if argument == orig_flag && !orig_flag.include?("=")
+                arguments.delete_at(i + 1)
+              end
             end
           end
-          arguments.delete(flag)
+          arguments.delete(orig_flag)
         end
       end
 

test/parser_test.rb
@@ -71,5 +71,10 @@ describe Slop::Parser do
       @parser.parse %w(lee --name lee lee)
       assert_equal %w(lee lee), @parser.arguments
     end
+
+    it "correctly removes options that use =" do
+      @parser.parse %w(lee --name=lee lee)
+      assert_equal %w(lee lee), @parser.arguments
+    end
   end
 end