95b0c5da79a2fb58457ea9defd0d6eb2828d3f65
Lee Jarvis <leejarvis@fastmail.com>
2021-06-15 12:21:28 +0100

    Fix tests

--- test/slop_test.rb
+++ test/slop_test.rb
@@ -13,7 +13,7 @@ describe Slop do
 
   describe ".option_defined?" do
     it "handles bad constant names" do
-      assert_equal true, Slop.option_defined?("Foo?Bar")
+      assert_equal false, Slop.option_defined?("Foo?Bar")
     end
 
     it "returns false if the option is not defined" do
